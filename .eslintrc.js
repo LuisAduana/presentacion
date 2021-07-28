243d8b0e3a4a1f7bd29934847e534fcba5c99c05
LuisAduana <38675549+LuisAduana@users.noreply.github.com>
2021-07-28 04:15:54 -0500

    Configuracion de ESLint y Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
